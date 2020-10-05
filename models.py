139111e64c259b0f86d08e53dffd0c2962350bac
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2020-10-05 13:40:58 -0600

    Primera Fase de darrollo

---
+++ models.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Pais(db.Model):
+     __tablename__ = 'country'
+     country_id = db.Column(db.Integer,primary_key=True)
+     country = db.Column(db.String())
+     last_update = db.Column(db.String())
+     def __init__(self,country,last_update):
+          self.country = country
+          self.last_update = last_update
+     def __repr__(self):
+         return '<country_id{}>'.format(self.country_id)
