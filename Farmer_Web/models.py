f8ecf3cdfd9b10119ceec3ce91ec6d005dae6e97
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2020-10-05 19:22:33 -0600

    Aplicacion Funciona, solo quitar la con sulta de clinete funciona lo demas

--- Farmer_Web/models.py
+++ Farmer_Web/models.py
@@ -11,3 +11,63 @@ class Pais(db.Model):
           self.last_update = last_update
      def __repr__(self):
          return '<country_id{}>'.format(self.country_id)
+
+class Ciudad(db.Model):
+     __tablename__ = 'city'
+     city_id = db.Column(db.Integer,primary_key=True)
+     city = db.Column(db.String())
+     country_id = db.Column(db.Integer,db.ForeignKey('country_id'))
+     last_update = db.Column(db.String())
+     def __init__(self,city,country_id,last_update):
+          self.city = city
+          self.country_id =country_id
+          self.last_update =last_update
+     def __repr__(self):
+          return 'city_id{}'.format(self.city_id)  
+
+
+class Address(db.Model):
+     __tablename__ = 'address'
+     address_id = db.Column(db.Integer,primary_key=True)
+     address = db.Column(db.String())
+     address2 = db.Column(db.String())
+     district = db.Column(db.String())
+     city_id = db.Column(db.Integer,db.ForeignKey('city_id'))
+     postal_code = db.Column(db.String())
+     phone = db.Column(db.String())
+     last_update = db.Column(db.String())
+     def __init__(self,address,address2,district,city_id,postal_code,phone,last_update):
+          self.address = address
+          self.address2 = address2
+          self.district = district
+          self.city_id  = city_id
+          self.postal_code = postal_code
+          self.phone = phone
+          self.last_update = last_update
+     def __repr__(self):
+         return '<address_id{}>'.format(self.address_id)
+
+class Cliente(db.Model):
+     __tablename__ = 'customer'
+     customer_id = db.Column(db.Integer,primary_key=True)
+     store_id = db.Column(db.Integer)
+     first_name = db.Column(db.String())
+     last_name = db.Column(db.String())
+     email = db.Column(db.String())
+     address_id =db.Column(db.Integer,db.ForeignKey('address_id'))
+     activebool = db.Column(db.Boolean)
+     create_date = db.Column(db.String())
+     last_update = db.Column(db.String())
+     active = db.Column(db.Integer())
+     def __init__(self,store_id,first_name,last_name,email,address_id,activebool,create_date,last_update,active):
+          self.store_id = store_id
+          self.first_name = first_name
+          self.last_name = last_name
+          self.email = email
+          self.address_id = address_id
+          self.activebool = activebool
+          self.create_date = create_date
+          self.last_update = last_update
+          self.active = active
+     def __repr__(self):
+          return '<customer_id{}>'.format(self.customer_id)    
--- Farmer_Web/routes.py
+++ Farmer_Web/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template,url_for,json,jsonify
 from Farmer_Web import app
-from Farmer_Web.models import Pais
+from Farmer_Web.models import Pais,Ciudad,Address,Cliente
 
 @app.route('/')
 def method_name():
@@ -21,3 +21,18 @@ def dtoke():
     papo = [{"country_id":cachita.country_id,"country":cachita.country,"last_update": cachita.last_update} for cachita in cacha]
     dumx = json.dumps(papo)
     return jsonify({'data':papo})
+
+@app.route('/Clients')
+def clientela():
+    return render_template("country.html")
+
+@app.route('/Clients/raws')
+def clientss():
+    return render_template("country.html")
+
+@app.route('/Clients/paises')
+def numero_clientes():
+    cole = Cliente.query.with_entities(Pais.country,func.count(Cliente.customer_id).label('n_clientes')).join(Address,Cliente.address_id == Address.address_id).join(Ciudad,Address.city_id==Ciudad.city_id).join(Pais,Ciudad.country_id==Pais.country_id).group_by(Pais.country)
+    kendrick = [{"Pais":c.country,"NumeroC":c.n_clientes} for c in cole]
+    dup = json.dumps(kendrick)
+    return jsonify({'Data':kendrick})    
